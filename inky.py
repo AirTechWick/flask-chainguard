cf4b77d142b7ed691fcb87ad6642ea1076867dfe
Erik <erikbolt11@gmail.com>
2024-03-25 13:38:22 -0700

    Added tailwind

--- inky.py
+++ inky.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"Hey there python!\"}"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("4000"))
